f85b46efae4f9002f344c85e566c642d4d3427b0
yangleiqing0 <lei.yang@enmotech.com>
2019-10-18 17:39:34 +0800

    10.18 1：支持请求结果解析为0 2：修复插入数据库失败崩溃

--- common/regist_variables.py
+++ common/regist_variables.py
@@ -12,80 +12,82 @@ def to_regist_variables(name, method, url, data, headers, regist_variable='', re
     user_id = session.get('user_id')
     if 'html' in response_body:
         response_body = '<xmp> %s </xmp>' % response_body
-    # print('response_body:', response_body.encode('utf-8').decode('gbk'))
-    if regist_variable:
-        # 判断是否有注册变量和正则方法，有的话进行获取
-        if regular:
-            #  判断是否有正则匹配规则
-            regular_list = regular.split(',')
-            regist_variable_list = regist_variable.split(',')
-            print('regular_list:', regular_list, len(regular_list), len(regist_variable_list))
-            if len(regular_list) <= len(regist_variable_list):
-                # 判断正则和注册变量数目是否相符 小于或等于
-                regist_variable_value_list = []
-                for index in range(len(regular_list)):
-                    # 循环取正则的规则
-                    if '$.' in regular_list[index]:
-                        if not is_json(response_body):
-                            regist_variable_value = "不是合法的字典响应信息"
-                        else:
-                            keys = regular_list[index][2:].split('.')
-                            if '' in keys:
-                                keys.remove('')
-                            print('keys:', keys)
-                            regist_variable_value = json.loads(response_body)
-                            for key in keys:
-                                if key:
-                                    try:
-                                        if ']' in key and '[' in key:
-                                            key, _index = key.split('[')
-                                            regist_variable_value = regist_variable_value.get(key)[int(_index[:-1])]
-                                        else:
-                                            regist_variable_value = regist_variable_value.get(key)
-                                    except AttributeError as e:
-                                        print(e)
-                                        regist_variable_value = ''
-                                    print('regist_variable_value:', regist_variable_value)
-                    elif '$[' in regular_list[index]:
-                        # try:
-                            print('session params', testcase_name, session[testcase_name])
-                            p_index = int(regular_list[index][2:-1])
-                            if testcase_name != '__testcase_name':
-                                regist_variable_value = session[testcase_name][p_index]
+    print('response_body:', response_body)
+    try:
+        if regist_variable:
+            # 判断是否有注册变量和正则方法，有的话进行获取
+            if regular:
+                #  判断是否有正则匹配规则
+                regular_list = regular.split(',')
+                regist_variable_list = regist_variable.split(',')
+                print('regular_list:', regular_list, len(regular_list), len(regist_variable_list))
+                if len(regular_list) <= len(regist_variable_list):
+                    # 判断正则和注册变量数目是否相符 小于或等于
+                    regist_variable_value_list = []
+                    for index in range(len(regular_list)):
+                        # 循环取正则的规则
+                        if '$.' in regular_list[index]:
+                            if not is_json(response_body):
+                                regist_variable_value = "不是合法的字典响应信息"
                             else:
-                                regist_variable_value = '__testcase_name'
-                        # except Exception as e:
-                        #     regist_variable_value = str(e)
-                    else:
-                        try:
-                            regist_variable_value = re.compile(regular_list[index]).findall(response_body)
-                        except Exception as e:
-                            regist_variable_value = str(e)
+                                keys = regular_list[index][2:].split('.')
+                                if '' in keys:
+                                    keys.remove('')
+                                print('keys:', keys)
+                                regist_variable_value = json.loads(response_body)
+                                for key in keys:
+                                    if key:
+                                        try:
+                                            if ']' in key and '[' in key:
+                                                key, _index = key.split('[')
+                                                regist_variable_value = regist_variable_value.get(key)[int(_index[:-1])]
+                                            else:
+                                                regist_variable_value = regist_variable_value.get(key)
+                                        except AttributeError as e:
+                                            print(e)
+                                            regist_variable_value = ''
+                                        print('regist_variable_value:', regist_variable_value)
+                        elif '$[' in regular_list[index]:
+                            # try:
+                                print('session params', testcase_name, session[testcase_name])
+                                p_index = int(regular_list[index][2:-1])
+                                if testcase_name != '__testcase_name':
+                                    regist_variable_value = session[testcase_name][p_index]
+                                else:
+                                    regist_variable_value = '__testcase_name'
+                            # except Exception as e:
+                            #     regist_variable_value = str(e)
+                        else:
+                            try:
+                                regist_variable_value = re.compile(regular_list[index]).findall(response_body)
+                            except Exception as e:
+                                regist_variable_value = str(e)
 
-                    if not regist_variable_value:
-                        regist_variable_value = ''
-                    elif isinstance(regist_variable_value, (int, str, dict)):
-                        pass
-                    elif len(regist_variable_value) > 0:
-                        regist_variable_value = regist_variable_value[0]
-                    else:
-                        regist_variable_value = '未知的值'
-                    regist_variable_value_list.append(regist_variable_value)
-                    if Variables.query.filter(Variables.name == regist_variable_list[index], Variables.user_id == user_id).count() > 0:
-                        print('%s 请求结束,存在此变量时：' % url,
-                              Variables.query.filter(Variables.name == regist_variable_list[index], Variables.user_id == user_id).first())
-                        Variables.query.filter(Variables.name == regist_variable_list[index], Variables.user_id == user_id).first().value = \
-                          regist_variable_value
-                        if is_commit:
+                        if isinstance(regist_variable_value, (int, str, dict)) or regist_variable_value==0:
+                            pass
+                        elif not regist_variable_value:
+                            regist_variable_value = ''
+                        elif len(regist_variable_value) > 0:
+                            regist_variable_value = regist_variable_value[0]
+                        else:
+                            regist_variable_value = '未知的值'
+                        regist_variable_value_list.append(regist_variable_value)
+                        if Variables.query.filter(Variables.name == regist_variable_list[index], Variables.user_id == user_id).count() > 0:
+                            print('%s 请求结束,存在此变量时：' % url,
+                                  Variables.query.filter(Variables.name == regist_variable_list[index], Variables.user_id == user_id).first())
+                            Variables.query.filter(Variables.name == regist_variable_list[index], Variables.user_id == user_id).first().value = \
+                              regist_variable_value
                             db.session.commit()
-                    else:
-                        private_variable_value = regist_variable_value
-                        private_variable = Variables(regist_variable_list[index], private_variable_value, is_private=1,
-                                                     user_id=user_id)
-                        db.session.add(private_variable)
-                        if is_commit:
+                        else:
+                            private_variable_value = regist_variable_value
+                            private_variable = Variables(regist_variable_list[index], private_variable_value, is_private=1,
+                                                         user_id=user_id)
+                            db.session.add(private_variable)
                             db.session.commit()
-                return response_body, str(regist_variable_value_list)
-            return response_body, '正则匹配规则数量过多'
-        return response_body, '未存在正则匹配'
-    return response_body, '存在未注册变量'
+                    return response_body, str(regist_variable_value_list)
+                return response_body, '正则匹配规则数量过多'
+            return response_body, '未存在正则匹配'
+        return response_body, '存在未注册变量'
+    except Exception as e:
+        return response_body, '注册变量解析失败'
+
